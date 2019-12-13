b65e6c67e620f676a5f15aaf9970e1b7b8bacb7e
julienPalleau <jpalleau@gmail.com>
2019-12-13 17:01:51 +0100

    Problem1 added

--- ObjectOrientedProgramming/Exercices.py
+++ ObjectOrientedProgramming/Exercices.py
@@ -1,13 +1,31 @@
 """ Problem 1 """
+import math
 
 
 class line:
 
     def __init__(self, coor1, coor2):
-        pass
+        self.x1 = coor1[0]
+        self.x2 = coor2[0]
+        self.y1 = coor1[1]
+        self.y2 = coor2[1]
 
     def distance(self):
-        pass
+        distance = math.sqrt((self.x2 - self.x1) ** 2 +
+                             (self.y2 - self.y1) ** 2)
+        return distance
 
     def slope(self):
-        pass
+        slope = (self.y2 - self.y1)/(self.x2 - self.x1)
+        return slope
+
+
+coordinate1 = (3, 2)
+coordinate2 = (8, 10)
+li = line(coordinate1, coordinate2)
+
+print(line.distance(li))
+print(line.slope(li))
+
+
+""" Problem 2 """
