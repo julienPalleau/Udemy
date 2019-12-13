9a5baa48ba0fda808ff2a83c571288be300b0d05
julienPalleau <jpalleau@gmail.com>
2019-12-13 17:17:53 +0100

    Problem2 added

--- ObjectOrientedProgramming/Exercices.py
+++ ObjectOrientedProgramming/Exercices.py
@@ -29,3 +29,24 @@ print(line.slope(li))
 
 
 """ Problem 2 """
+
+
+class Cylinder:
+    pi = 3.14
+
+    def __init__(self, height=1, radius=1):
+        self.h = height
+        self.r = radius
+
+    def volume(self):
+        volume = Cylinder.pi * self.r ** 2 * self.h
+        return volume
+
+    def surface_area(self):
+        aire = 2 * Cylinder.pi * self.r * (self.h + self.r)
+        return aire
+
+
+c = Cylinder(2, 3)
+print(Cylinder.volume(c))
+print(Cylinder.surface_area(c))
