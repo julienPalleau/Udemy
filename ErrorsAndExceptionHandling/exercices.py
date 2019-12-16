93e4ea85d3696f6ce8ab279bc45d6b43c88d5734
julienPalleau <jpalleau@gmail.com>
2019-12-16 14:53:29 +0100

    added the last exercices

--- ErrorsAndExceptionHandling/exercices.py
+++ ErrorsAndExceptionHandling/exercices.py
@@ -1,31 +1,35 @@
 """ Problem 1 """
-for i in ['a', 'b', 'c']:
-    try:
+
+try:
+    for i in ['a', 'b', 'c']:
         print(i**2)
-    except TypeError:
-        print("erreur i n'est pas un entier!")
+except TypeError:
+    print("erreur i n'est pas un entier!")
+
 
 """ Problem 2 """
-x = 5
-y = 0
+
 try:
+    x = 5
+    y = 0
     z = x/y
 except ZeroDivisionError:
     print("La division par 0 est impossible !")
 finally:
     print("All Done")
 
+
 """ Problem 3 """
 
 
 def ask():
-    while(True):
+    while True:
         try:
-            result = int(input("Veuillez saisir un entier : "))
+            n = int(input("Veuillez saisir un entier : "))
         except:
             print("Svp veuillez saisir un entier ex: 1,2,3 ... n")
         else:
-            return f"Le resultat de l'entier eleve au carre est {result**2}"
+            return f"Your number squared is: {n**2}"
 
 
 print(ask())
