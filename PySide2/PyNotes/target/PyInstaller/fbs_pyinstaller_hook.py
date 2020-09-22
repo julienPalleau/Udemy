96b1a043b7affeb55ccd6dfa6b0e7584b2c7236e
julienPalleau <jpalleau@gmail.com>
2020-09-22 17:48:08 +0200

    cleaning files to keep sources only

--- PySide2/PyNotes/target/PyInstaller/fbs_pyinstaller_hook.py
+++
@@ -1,3 +0,0 @@
-import importlib
-module = importlib.import_module('fbs_runtime._frozen')
-module.BUILD_SETTINGS = {'app_name': 'PyNotes', 'author': 'Julien Palleau', 'version': '0.0.0', 'environment': 'local'}
