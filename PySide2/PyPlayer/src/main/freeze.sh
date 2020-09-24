efe1591c1905daf0b2e235ef58a07472d813e5bc
julienPalleau <jpalleau@gmail.com>
2020-09-24 08:28:14 +0200

    initial commit

---
+++ PySide2/PyPlayer/src/main/freeze.sh
@@ -0,0 +1,4 @@
+source ~/venv/Scripts/activate
+cd ~/Documents/GitHub/Udemy/PySide2/PyPlayer
+fbs clean
+fbs freeze
---
+++ PySide2/PyPlayer/src/main/python/package/main_window.py
@@ -0,0 +1,30 @@
+from PySide2 import QtWidgets
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("PyPlayer")
+        self.setup_ui()
+
+    def setup_ui(self):
+        self.create_widgets()
+        self.modify_widgets()
+        self.create_layout()
+        self.add_widgets()
+        self.setup_connections()
+
+    def create_widgets(self):
+        pass
+
+    def modify_widgets(self):
+        pass
+
+    def create_layout(self):
+        pass
+
+    def add_widgets(self):
+        pass
+
+    def setup_connections(self):
+        pass
