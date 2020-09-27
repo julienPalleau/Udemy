1bf520d019105c2f361dc7533cf937130a22ed8a
juju <jpalleau@gmail.com>
2020-09-27 17:47:32 +0200

    added - Gestion du clique sur le QTreeView

--- PySide2/PyExplorer/src/main/python/main.py
+++ PySide2/PyExplorer/src/main/python/main.py
@@ -6,7 +6,7 @@ from package.main_window import MainWindow
 
 if __name__ == '__main__':
     appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
-    window = MainWindow()
+    window = MainWindow(ctx=appctxt)
     window.resize(1920/2, 1200/2)
     window.show()
     exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
--- PySide2/PyExplorer/src/main/python/package/main_window.py
+++ PySide2/PyExplorer/src/main/python/package/main_window.py
@@ -2,8 +2,9 @@ from PySide2 import QtWidgets, QtCore
 
 
 class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self):
+    def __init__(self, ctx):
         super().__init__()
+        self.ctx = ctx
         self.setWindowTitle("PyExplorer")
         self.setup_ui()
 
@@ -13,6 +14,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.create_layouts()
         self.add_widgets_to_layouts()
         self.setup_connections()
+        self.create_file_model()
 
     def create_widgets(self):
         self.toolbar = QtWidgets.QToolBar()
@@ -27,7 +29,6 @@ class MainWindow(QtWidgets.QMainWindow):
     def create_layouts(self):
         self.main_layout = QtWidgets.QHBoxLayout(self.main_widget)
 
-
     def add_widgets_to_layouts(self):
         self.addToolBar(QtCore.Qt.TopToolBarArea, self.toolbar)
         self.setCentralWidget(self.main_widget)
@@ -36,5 +37,19 @@ class MainWindow(QtWidgets.QMainWindow):
         self.main_layout.addWidget(self.sld_iconSize)
 
     def setup_connections(self):
-        pass
+        self.tree_view.clicked.connect(self.treeview_clicked)
 
+    def create_file_model(self):
+        self.model = QtWidgets.QFileSystemModel()
+        root_path = QtCore.QDir.rootPath()
+        self.model.setRootPath(root_path)
+        self.tree_view.setModel(self.model)
+        self.list_view.setModel(self.model)
+        self.list_view.setRootIndex(self.model.index(root_path))
+        self.tree_view.setRootIndex(self.model.index(root_path))
+
+    def treeview_clicked(self, index):
+        if self.model.isDir(index):
+            self.list_view.setRootIndex(index)
+        else:
+            self.list_view.setRootIndex(index.parent())
