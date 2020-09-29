339bf8a98a2138b233e1336ce4737f7b7357653c
juju <jpalleau@gmail.com>
2020-09-29 10:50:16 +0200

    sync

--- PySide2/PyConverter/src/main/python/main.py
+++ PySide2/PyConverter/src/main/python/main.py
@@ -1,13 +1,26 @@
-from fbs_runtime.application_context.PySide2 import ApplicationContext
+from fbs_runtime.application_context.PySide2 import ApplicationContext, cached_property
+from PySide2 import QtGui
 
 import sys
 
 from package.main_window import MainWindow
 
+
+class AppContext(ApplicationContext):
+    def run(self):
+        main_window = MainWindow(ctx=self)
+        main_window.resize(1920 / 4, 1200 / 2)
+        main_window.show()
+        return self.app.exec_()
+
+    @cached_property
+    def img_checked(self):
+        return QtGui.QIcon(self.get_resource("images/checked.png"))
+
+    @cached_property
+    def img_unchecked(self):
+        return QtGui.QIcon(self.get_resource("images/unchecked.png"))
+
 if __name__ == '__main__':
-    appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
-    window = MainWindow()
-    window.resize(1920/2, 1200/2)
-    window.show()
-    exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
-    sys.exit(exit_code)
+    appctxt = AppContext()
+    sys.exit(appctxt.run())
--- PySide2/PyConverter/src/main/python/package/main_window.py
+++ PySide2/PyConverter/src/main/python/package/main_window.py
@@ -1,9 +1,10 @@
-from PySide2 import QtWidgets
+from PySide2 import QtWidgets, QtCore
 
 
-class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self):
+class MainWindow(QtWidgets.QWidget):
+    def __init__(self, ctx):
         super().__init__()
+        self.ctx = ctx
         self.setWindowTitle("PyConverter")
         self.setup_ui()
 
@@ -15,17 +16,66 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setup_connections()
 
     def create_widgets(self):
-        pass
+        self.lbl_quality = QtWidgets.QLabel("Qualite:")
+        self.spn_quality = QtWidgets.QSpinBox()
+        self.lbl_size = QtWidgets.QLabel("Taille:")
+        self.spn_size = QtWidgets.QSpinBox()
+        self.lbl_dossierOut = QtWidgets.QLabel("Dossier de sortie:")
+        self.le_dossierOut = QtWidgets.QLineEdit()
+        self.lw_files = QtWidgets.QListWidget()
+        self.btn_convert = QtWidgets.QPushButton("Conversion")
+        self.lbl_dropInfo = QtWidgets.QLabel("^ Deposez les images sur l'interface")
 
     def modify_widgets(self):
-        pass
+        css_file = self.ctx.get_resource("style.css")
+        with open(css_file, "r") as f:
+            self.setStyleSheet(f.read())
+
+        # Alignement
+        self.spn_quality.setAlignment(QtCore.Qt.AlignRight)
+        self.spn_size.setAlignment(QtCore.Qt.AlignRight)
+        self.le_dossierOut.setAlignment(QtCore.Qt.AlignRight)
+
+        # Range
+        self.spn_quality.setRange(1, 100)
+        self.spn_quality.setValue(75)
+        self.spn_size.setRange(1, 100)
+        self.spn_size.setValue(50)
+
+        # Divers
+        self.le_dossierOut.setPlaceholderText("Dossier de sortie...")
+        self.le_dossierOut.setText("reduced")
+        self.lbl_dropInfo.setVisible(False)
+
+        self.setAcceptDrops(True)  # on accepte que l'on puisse deposer des element sur l'interface
 
     def create_layouts(self):
-        pass
+        self.main_layout = QtWidgets.QGridLayout(self)
 
     def add_widgets_to_layouts(self):
-        pass
+        self.main_layout.addWidget(self.lbl_quality, 0, 0, 1, 1)
+        self.main_layout.addWidget(self.spn_quality, 0, 1, 1, 1)
+        self.main_layout.addWidget(self.lbl_size, 1, 0, 1, 1)
+        self.main_layout.addWidget(self.spn_size, 1, 1, 1, 1)
+        self.main_layout.addWidget(self.lbl_dossierOut, 2, 0, 1, 1)
+        self.main_layout.addWidget(self.le_dossierOut, 2, 1, 1, 1)
+        self.main_layout.addWidget(self.lw_files, 3, 0, 1, 2)
+        self.main_layout.addWidget(self.lbl_dropInfo, 4, 0, 1, 2)
+        self.main_layout.addWidget(self.btn_convert, 5, 0, 1, 2)
 
     def setup_connections(self):
         pass
 
+    def dragEnterEvent(self, event):
+        self.lbl_dropInfo.setVisible(True)
+        event.accept()
+
+    def dragLeaveEvent(self, event):
+        self.lbl_dropInfo.setVisible(False)
+
+    def dropEvent(self, event):
+        event.accept()
+        for url in event.mimeData().urls():
+            self.lw_files.addItem(url.toLocalFile())
+
+        self.lbl_dropInfo.setVisible(False)
