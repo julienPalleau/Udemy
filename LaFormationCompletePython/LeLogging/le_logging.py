dd218af9e93aad1803d89129924e171a38ebf339
juju <jpalleau@gmail.com>
2020-10-14 08:07:19 +0200

    added - Configurerer le logger et ecrire dans un fichier de log

--- LaFormationCompletePython/LeLogging/le_logging.py
+++ LaFormationCompletePython/LeLogging/le_logging.py
@@ -1,7 +1,24 @@
 import logging
+""" on definit ci-dessous le niveau de debug que l'on souhaite
+ par defaut le niveau est sur warning, et le set à DEBUG"""
+#logging.basicConfig(level=logging.DEBUG)
+
+"""
+level : parametre pour decrire le niveau de log desire. Choix entre
+debug, info, warning, error et critical.
+
+filename et filemode(w, a): Les parametres pour ecrire dans un fichier
+
+format: Le parametre format afin de formatter le fichier affiché
+"""
+
+logging.basicConfig(level=logging.DEBUG,
+                    filename="app.log",
+                    filemode="a",
+                    format='%(asctime)s : %(levelname)s : %(message)s')
 
 logging.debug("La fonction a bien été exécutée")
 logging.info("Message d'information général")
 logging.warning("Attention !")
 logging.error("Une erreur est arrivée")
-logging.error("Erreur critique")
+logging.critical("Erreur critique")
