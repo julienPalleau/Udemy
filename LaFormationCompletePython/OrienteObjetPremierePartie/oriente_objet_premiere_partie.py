84c70123ee058899f88174829cc4fb1e752ad846
juju <jpalleau@gmail.com>
2020-10-15 18:20:01 +0200

    oriente objet premiere partie

--- LaFormationCompletePython/OrienteObjetPremierePartie/oriente_objet_premiere_partie.py
+++ LaFormationCompletePython/OrienteObjetPremierePartie/oriente_objet_premiere_partie.py
@@ -1,1 +1,53 @@
-print("hello world!")
+class Voiture:
+
+    def __init__(self):
+        self.essence = 100
+
+    def afficher_reservoir(self):
+        print(f"La voiture contient {self.essence}L d'essence")
+
+    def faire_le_plein(self):
+        self.essence = 100
+        print(f"Vous pouvez repartir !")
+
+    def roule(self, distance):
+        self.essence = self.essence - (distance * 5) / 100
+
+        if self.essence <= 0:
+            print("Vous n'avez plus d'essence, faites le plein !")
+            return
+
+        elif self.essence < 10:
+            print(f"Vous n'avez bientot plus d'essence !")
+
+        self.afficher_reservoir()
+
+voiture_01 = Voiture()
+#voiture_01.essence
+#voiture_01.afficher_reservoir()
+#voiture_01.roule(100)
+
+# Correction
+
+# class Voiture:
+#     def __init__(self):
+#         self.essence = 100
+#
+#     def afficher_reservoir(self):
+#         print(f"La voiture contient {self.essence}L d'essence.")
+#
+#     def roule(self, km):
+#         if self.essence <= 0:
+#             print("Vous n'avez plus d'essence, faites le plein !")
+#             return
+#
+#         self.essence -= (km * 5) / 100
+#
+#         if self.essence < 10:
+#             print("Vous n'avez bientôt plus d'essence !")
+#
+#         self.afficher_reservoir()
+#
+#     def faire_le_plein(self):
+#         self.essence = 100
+#         print("Vous pouvez repartir !")
