2bd6582bffd79dc426956b2ebbec5956d44c5dff
juju <jpalleau@gmail.com>
2020-11-04 20:19:19 +0100

    Tp Les Raccourcis claviers

---
+++ PySide1/LesInterfacesGraphiquesAvecPyside/LesShortcuts.py
@@ -0,0 +1,27 @@
+from PySide2 import QtWidgets, QtGui
+
+
+class FenetrePrincipale(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('Mon Application')
+
+        layout = QtWidgets.QHBoxLayout(self)
+
+        self.lw_demo = QtWidgets.QListWidget(self)
+        self.lw_demo.addItems(['Premier', 'Deuxieme', 'Troisieme'])
+
+        QtWidgets.QShortcut(QtGui.QKeySequence('Ctrl+Delete'), self, self.lw_demo.clear)
+        QtWidgets.QShortcut(QtGui.QKeySequence('Esc'), self, self.close)
+
+        QtGui.QKeySequence(QtGui.QKeySequence.Print)
+        QtGui.QKeySequence("Ctrl+P")
+
+        layout.addWidget(self.lw_demo)
+
+
+app = QtWidgets.QApplication([])
+fenetre = FenetrePrincipale()
+fenetre.show()
+app.exec_()
